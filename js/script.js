e4b1485c2cc4c46244cef5e60bcc9f390c2ababe
Ghezlene Doballah <ghyzounette@gmail.com>
2020-01-25 11:34:59 +0100

    search-contacts

--- js/script.js
+++ js/script.js
@@ -3,17 +3,21 @@ $(document).ready(
     $('#send').click(
       function() {
       sendMessage();
-      setTimeout(function(){ receiveMessage(); }, 1000);
       }
     );
-    // $('#chat-box').keypress(
-    //   function(event) {
-    //     if (event.which == 13) {
-    //       sendMessage();
-    //       setTimeout(function(){ receiveMessage(); }, 1000);
-    //     }
-    //   }
-    // );
+    $('#chat-box').keypress(
+      function(event) {
+        if (event.which == 13) {
+          sendMessage();
+        }
+      }
+    );
+    $('.search .search-bar').keydown(
+      function(){
+        var text = $('.search .search-bar').val().toLowerCase();
+        searchContacts(text);
+      }
+    );
   }
 );
 
@@ -34,22 +38,29 @@ function sendMessage() {
     newMessage.addClass('sent');
     $('.chat-window').append(newMessage);
     $('#chat-box').val('');
+    var response = 'ok';
+    var newResponse = $('.template .messages').clone();
+    newResponse.find('.message-text').text(response);
+    newResponse.find('.message-time').text(time);
+    newResponse.addClass('received');
+    setTimeout(function(){ $('.chat-window').append(newResponse); }, 1000);
   }
 }
+// Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+function searchContacts(string) {
+    $('.contact-element').hide();
+    for (var i = 0; i < $('.contact-element').length; i++) {
+      var name = $('.contact-element').eq(i).find('.contact-name').text().toLowerCase();
+      console.log(name);
 
-function receiveMessage() {
-  var response = 'ok';
-  var newResponse = $('.template .messages').clone();
-  newResponse.find('.message-text').text(response);
-  var data = new Date();
-  var hours = addZero(data.getHours());
-  var minutes = addZero(data.getMinutes());
-  var time = hours + ':' + minutes;
-  newResponse.find('.message-time').text(time);
-  newResponse.addClass('received');
-  $('.chat-window').append(newResponse);
-  $('#chat-box').val('');
-}
+      if (name.includes(string)) {
+        $('.contact-element').eq(i).show();
+      }
+
+    }
+
+  }
+
 
 
 function addZero(number) {
